dc312e4799406f8123be3ca63d91abdc8b75e412
Saurabh Singh <saurabhsingh0990@gmail.com>
2025-07-27 14:58:28 +0530

    time based cache expiry logic

--- include/caching.hpp
+++ include/caching.hpp
@@ -1,17 +1,22 @@
-#ifndef CACHE_HPP
-#define CACHE_HPP
+#ifndef CACHING_HPP
+#define CACHING_HPP
 
 #include <string>
 #include <unordered_map>
+#include <chrono>
 
 class SimpleCache {
 public:
+    SimpleCache();                   // Default constructor
+    SimpleCache(int ttl);           // Constructor with TTL in seconds
+
+    bool contains(const std::string& key) ;
     std::string get(const std::string& key);
-    void put(const std::string& key, const std::string& response);
-    bool contains(const std::string& key) const;
+    void put(const std::string& key, const std::string& value);
 
 private:
-    std::unordered_map<std::string, std::string> cache_;
+    int ttlSeconds = 60; // Default TTL (60 seconds)
+    std::unordered_map<std::string, std::pair<std::string, std::chrono::steady_clock::time_point>> cache_;
 };
 
-#endif
+#endif // CACHING_HPP
--- src/caching.cpp
+++ src/caching.cpp
@@ -1,13 +1,38 @@
+#include <iostream>
 #include "caching.hpp"
+#include <chrono>
+using namespace std;
+
+SimpleCache::SimpleCache() : ttlSeconds(60) {}  // default TTL to 60s
+
+SimpleCache::SimpleCache(int ttl) : ttlSeconds(ttl) {}
+
+bool SimpleCache::contains(const std::string& key) {
+    auto it = cache_.find(key);
+    if (it != cache_.end()) {
+        auto now = std::chrono::steady_clock::now();
+        auto age = std::chrono::duration_cast<std::chrono::seconds>(now - it->second.second);
+
+        if (age.count() < ttlSeconds) {
+            cout << "[CACHE] HIT: " << key << "\n";
+            return true;
+        } else {
+            cout << "[CACHE] EXPIRED: " << key << "\n";
+            cache_.erase(it); // remove expired
+        }
+    }
+    cout << "[CACHE] MISS: " << key << "\n";
+    return false;
+}
+
 
 std::string SimpleCache::get(const std::string& key) {
-    return cache_.at(key);
+    if (contains(key)) {
+        return cache_[key].first;
+    }
+    return "";
 }
 
-void SimpleCache::put(const std::string& key, const std::string& response) {
-    cache_[key] = response;
-}
-
-bool SimpleCache::contains(const std::string& key) const {
-    return cache_.find(key) != cache_.end();
+void SimpleCache::put(const std::string& key, const std::string& value) {
+    cache_[key] = {value, std::chrono::steady_clock::now()};
 }
